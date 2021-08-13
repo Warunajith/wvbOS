17ab669efcebd61966140809e4f83cc8db5a6419
Warunajith Bandara <62562726+Warunajith@users.noreply.github.com>
2021-08-13 17:05:06 +0530

    Do the memory segmentation by calling function

--- kmain.c
+++ kmain.c
@@ -1,10 +1,11 @@
 /* The C function */
    #include "drivers/frame_buffer.h"
    #include "drivers/serial_port.h"
+   #include "memory_segments.h"
 
     void kmain()
     {
-    
+            segments_install_gdt();
             char ptr2[] = "Welcome to WVBOS";
             serial_write(SERIAL_COM1_BASE, ptr2, 26);
             fb_write(ptr2, 19);
