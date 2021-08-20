467d9002cb916a1b340c4b41b33eefe117aa2a83
Warunajith Bandara <62562726+Warunajith@users.noreply.github.com>
2021-08-20 18:20:57 +0530

    Drivers updated

--- drivers/io.h
+++ drivers/io.h
@@ -1,4 +1,4 @@
-#ifndef INCLUDE_IO_H
+    #ifndef INCLUDE_IO_H
     #define INCLUDE_IO_H
 
     /** outb:
@@ -8,6 +8,13 @@
      *  @param data The data to send to the I/O port
      */
     void outb(unsigned short port, unsigned char data);
+
+    /** inb:
+     *  Read a byte from an I/O port.
+     *
+     *  @param  port The address of the I/O port
+     *  @return      The read byte
+     */
     unsigned char inb(unsigned short port);
 
     #endif /* INCLUDE_IO_H */
