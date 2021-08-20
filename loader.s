84fac2d4a85ce5fb75577dbf35f2ae7f7a4028a1
Warunajith Bandara <62562726+Warunajith@users.noreply.github.com>
2021-08-20 18:28:14 +0530

    Cause to "Error 13" has fixed

--- loader.s
+++ loader.s
@@ -11,7 +11,7 @@ global loader                   ; the entry symbol for ELF
     kernel_stack:                   ; label points to beginning of memory
         resb KERNEL_STACK_SIZE      ; reserve stack for the kernel 
 
-    section .text:                  ; start of the text (code) section
+    section .text                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
         dd FLAGS                    ; the flags,
