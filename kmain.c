b689c13c299ba7841b480ab7bccb2a372a15fdf6
Warunajith Bandara <62562726+Warunajith@users.noreply.github.com>
2021-08-27 11:11:20 +0530

    Finding the Program in Memory
    Jumping to the Code

--- kmain.c
+++ kmain.c
@@ -10,11 +10,30 @@
     void kmain()
     {
 
-       char arr[] = "Welcome to WVBOS";
+       /*char arr[] = "Welcome to WVBOS";
        fb_move_cursor(6*80);
        fb_write(arr, 20);
        serial_write(arr, 20);
        segments_install_gdt();
-       interrupts_install_idt();
+       interrupts_install_idt();*/
+     
+   multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+  	multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
+  	unsigned int address_of_module = modules->mod_start;
+  	
+  	if((mbinfo->mods_count) == 1){
+  		char message[] = "ONE module loaded successfully!";
+  		serial_write(message,sizeof(message));
+  		
+  		typedef void (*call_module_t)(void);
+        	/* ... */
+        	call_module_t start_program = (call_module_t) address_of_module;
+        	start_program();
+        	/* we'll never get here, unless the module code returns */
+
+  	}else{
+  		char message[] = "Error: More than ONE module loaded";
+  		serial_write(message,sizeof(message));
+  	}
 
     }
